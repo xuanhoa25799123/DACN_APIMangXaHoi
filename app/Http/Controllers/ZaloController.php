29c135ad171d2d5d6af923a89e8739076308e3e6
super3636 <ioeioe649764321@gmail.com>
2021-04-14 09:23:04 +0700

    asd

--- app/Http/Controllers/ZaloController.php
+++ app/Http/Controllers/ZaloController.php
@@ -66,5 +66,6 @@ class ZaloController extends Controller
         return redirect()->route('dashboard');
     }
 
+
 }
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [
     'uses'=>'App\Http\Controllers\ZaloController@login',
 ]);
+Route::get('/login', [
+    'as'=>'login',
+    'uses'=>'App\Http\Controllers\ZaloController@login'
+]);
+Route::get('/add/{id}', [
+    'as'=>'social.add',
+    'uses'=>'App\Http\Controllers\ZaloController@add'
+]);
+Route::post('/send-message}', [
+    'as'=>'social.sendmessage',
+    'uses'=>'App\Http\Controllers\ZaloController@sendmessage'
+]);
 Route::get('/dashboard', [
     'as'=>'dashboard',
     'uses'=>'App\Http\Controllers\ZaloController@dashboard'
+]);
+route::get('/dashboard/oa',[
+    'uses'=>'App\Http\Controllers\OAController@dashboard'
+]);
+
+Route::get('/oa/user', [
+    'as'=>'oa.user',
+    'uses'=>'App\Http\Controllers\OAController@login'
+]);
+
+
+Route::get('/test', [
+    'as'=>'test',
+    'uses'=>'App\Http\Controllers\ZaloController@test'
 ]);
