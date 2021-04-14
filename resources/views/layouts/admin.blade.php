5da29568b8c283478209cef10c7fe4f1a80cf3ba
super3636 <ioeioe649764321@gmail.com>
2021-04-14 20:12:20 +0700

    hehHE

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,12 +6,12 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     @yield('title')
     <link rel="stylesheet" href="{{asset('/css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/sidebar/style.css')}}">
+{{--    <link rel="stylesheet" href="{{asset('/css/sidebar/style.css')}}">--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     @yield('css')
 </head>
 <body>
-    @include('partials.sidebar')
+{{--    @include('partials.sidebar')--}}
     @yield('content')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
