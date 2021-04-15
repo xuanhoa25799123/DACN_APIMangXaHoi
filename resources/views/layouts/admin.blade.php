88b28358c48290380286b5b543808b7010c7e386
super3636 <ioeioe649764321@gmail.com>
2021-04-15 13:15:38 +0700

    heasd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,10 @@
     <link rel="stylesheet" href="{{asset('/css/sidebar/style.css')}}">
     <link rel="stylesheet" href="{{asset('/css/header/style.css')}}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
     @yield('css')
 </head>
 <body>
@@ -23,8 +26,7 @@
     </div>
 </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{asset('/js/main.js')}}"></script>
 @yield('js')
 </body>
 </html>
