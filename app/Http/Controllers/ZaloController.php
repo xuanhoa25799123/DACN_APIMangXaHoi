91cbae297b81981fec0c07a08b7d75b2de402635
super3636 <ioeioe649764321@gmail.com>
2021-04-15 13:38:36 +0700

    heasd

--- app/Http/Controllers/ZaloController.php
+++ app/Http/Controllers/ZaloController.php
@@ -55,7 +55,6 @@ class ZaloController extends Controller
     public function search($keyword)
     {
         $friendList = session('friends');
-        dd($friendList);
         $friends = array_slice($friendList,0,1);
         $html = view('partials.friends')->with(compact('friends'))->render();
         return response()->json(['success' => true, 'friends' => $friendList]);
