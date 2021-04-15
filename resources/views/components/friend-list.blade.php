712f14f6e8f764ad61cf62eddb1b2fdfa77f4842
super3636 <ioeioe649764321@gmail.com>
2021-04-15 17:53:59 +0700

    asd

--- resources/views/components/friend-list.blade.php
+++ resources/views/components/friend-list.blade.php
@@ -8,11 +8,12 @@
     <script src="{{asset('js/search.js')}}"></script>
     @endsection
 @section('content')
-        <p class="text">Đã có {{$total}} người bạn vào ứng dụng</p>
+
         <div class="search-container">
             <i class="search-logo fa fa-search"></i>
         <input type="text" name="search" id="search-bar" placeholder="Tìm kiếm tên bạn bè">
         </div>
+        <p class="text">Đã có {{$total}} người bạn vào ứng dụng</p>
         <div class="list-friends">
        @include('partials.friends')
         </div>
