0364d20d7646b1b91ee643c08f2b8e8b0769bf19
super3636 <ioeioe649764321@gmail.com>
2021-04-15 13:09:16 +0700

    heasd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{asset('/css/sidebar/style.css')}}">
     <link rel="stylesheet" href="{{asset('/css/header/style.css')}}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     @yield('css')
 </head>
 <body>
