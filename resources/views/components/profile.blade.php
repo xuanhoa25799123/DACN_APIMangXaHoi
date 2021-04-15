c8ebfacf350b2c804a70db9fefc034f8f4e12bc5
super3636 <ioeioe649764321@gmail.com>
2021-04-15 18:13:33 +0700

    asd

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -5,6 +5,13 @@
 @endsection
 @section('content')
     <div class="profile-container">
-
+        <div class="profile-header">
+            <div class="profile-background">
+                <img class="profile-img" alt ="" src="{{$profile['picture']['data']['url']}}">
+            </div>
+        </div>
+        <div class="footer">
+            <p class="name">{{$profile['name']}}</p>
+        </div>
     </div>
 @endsection
