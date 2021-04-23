b9f81cc9bf73adb37dd2df388c04bc08ad8245e6
super3636 <ioeioe649764321@gmail.com>
2021-04-23 18:41:05 +0700

    hehe

--- resources/views/partials/send-preview.blade.php
+++ resources/views/partials/send-preview.blade.php
@@ -3,8 +3,9 @@
     <img class="send-preview-profile" src="{{$profile['picture']['data']['url']}}" alt="">
     <div class="send-preview-content">
         <p>{{$message}}</p>
+        <div class="results">
     </div>
-    <div class="results">
+
 
     </div>
 </div>
