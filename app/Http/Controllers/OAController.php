87b0933d36156297ad0c600d58d8206025348c66
super3636 <ioeioe649764321@gmail.com>
2021-04-25 09:01:50 +0700

    hehe

--- app/Http/Controllers/OAController.php
+++ app/Http/Controllers/OAController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Zalo\Zalo;
+use Zalo\ZaloEndPoint;
 
 class OAController extends Controller
 {
@@ -10,7 +12,6 @@ class OAController extends Controller
     private $helper;
     public function __construct()
     {
-
         $config = array(
             'app_id'=>'2946516192240599353',
             'app_secret'=>'p31sk8862O3VA7z10dCf',
