18004d5b8f23afe8b0ee7cdd4b28ce3d2720ad52
super3636 <ioeioe649764321@gmail.com>
2021-04-25 08:43:34 +0700

    hehe

--- public/js/refresh-token.js
+++ public/js/refresh-token.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.refresh-token-btn').on('click',function(){
         let url = $(this).data('href');
         $.ajax({
-            url: 'https://zalo-api-app.herokuapp.com/refresh-token',
+            url: url,
             type: 'GET',
             dataType: 'jsonp',
             headers:{
