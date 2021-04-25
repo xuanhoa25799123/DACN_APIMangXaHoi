b4e15812b8645262822f3ddf290e9ac7bccda6c8
super3636 <ioeioe649764321@gmail.com>
2021-04-25 11:06:31 +0700

    hehe

--- resources/views/oa/partials/header.blade.php
+++ resources/views/oa/partials/header.blade.php
@@ -1,5 +1,5 @@
 <div class="header">
     <div class="profile">
-        <a href="{{}}"><img class="user-img" alt="" src="{{$oa_info['avatar']}}"></a>
+        <a href=""><img class="user-img" alt="" src="{{$oa_info['avatar']}}"></a>
     </div>
 </div>
