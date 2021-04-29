d8d50ee0caf2da0e46e60c9237cef0831614d198
super3636 <ioeioe649764321@gmail.com>
2021-04-29 13:24:54 +0700

    hehe

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,5 @@
 <div class="header">
-        <div class="profile"><img class="user-img" alt="" src="{{$profile['picture']['data']['url']}}"><p class="profile-name">{{$profile['name']}}</p></div>
-        @include('partials.menu-dropdown')
+        <div class="profile"><img class="user-img" alt="" src="{{$profile['picture']['data']['url']}}"><p class="profile-name">{{$profile['name']}}</p>
+            @include('partials.menu-dropdown')
+        </div>
 </div>
