feac5b5d41018123869c3d7698ae59c77a4eb46a
super3636 <ioeioe649764321@gmail.com>
2021-04-29 13:03:42 +0700

    hehe

--- resources/views/oa/dashboard.blade.php
+++ resources/views/oa/dashboard.blade.php
@@ -9,6 +9,10 @@
 
 @section('content')
     <div class="content">
+        <p>Tên: {{$oa_info['name']}}</p>
+        <p>Mô tả: {{$oa_info['description']}}</p>
+        <p>Avatar: <img alt="" src="{{$oa_info['avatar']}}" class="oa-avatar"></p>
+        <p>Cover: <img alt="" src="{{$oa_info['cover']}}"></p>
 </div>
 
 @endsection
