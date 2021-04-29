4619c63b142f4626499739ba2a0db6008996de40
super3636 <ioeioe649764321@gmail.com>
2021-04-29 12:55:37 +0700

    hehe

--- resources/views/oa/partials/header.blade.php
+++ resources/views/oa/partials/header.blade.php
@@ -1,5 +1,3 @@
 <div class="header">
-    <div class="profile">
-        <a href=""><img class="user-img" alt="" src="{{$oa_info['avatar']}}"><p>{{$oa_info['name']}}</p></a>
-    </div>
+        <a class="profile" href="/"><img class="user-img" alt="" src="{{$oa_info['avatar']}}"><p class="profile-name">{{$oa_info['name']}}</p></a>
 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,3 @@
 <div class="header">
-    <div class="profile">
-        <a href="{{route('profile')}}"><img class="user-img" alt="" src="{{$profile['picture']['data']['url']}}"></a>
-    </div>
+        <a class="profile" href="{{route('profile')}}"><img class="user-img" alt="" src="{{$profile['picture']['data']['url']}}"><p class="profile-name">{{$profile['name']}}</p></a>
 </div>
