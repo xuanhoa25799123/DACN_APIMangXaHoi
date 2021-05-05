e1a2c1546fb6554170e24dc2a7752acddd0d538a
super3636 <ioeioe649764321@gmail.com>
2021-05-05 09:47:13 +0700

    heheh

--- resources/views/oa/components/articles.blade.php
+++ resources/views/oa/components/articles.blade.php
@@ -25,7 +25,7 @@
             @foreach($articles as $index=>$article)
             <tr>
                 <th scope="row">{{$index}}</th>
-                <td>{{date('Y-m-d H:i:s',substr((string)$article->create_date,0,10))}} Cập nhật lần cuối vào {{date('Y-m-d H:i:s',substr((string)$article->update_date,0,10))}}</td>
+                <td>{{date('d-m-Y H:i:s',substr((string)$article->create_date,0,10))}} <p>Cập nhật lần cuối vào {{date('d-m-Y H:i:s',substr((string)$article->update_date,0,10))}}</p></td>
                 <td> <img class="article-image" src="{{$article->thumb}}" alt=""></td>
                 <td> {{$article->title}}</td>
                 <td> {{$article->total_view}}</td>
