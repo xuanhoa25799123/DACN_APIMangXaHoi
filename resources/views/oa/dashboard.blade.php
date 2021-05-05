2aa93ff2ff4a778178c90b611ceb310163dd8842
super3636 <ioeioe649764321@gmail.com>
2021-05-05 08:26:37 +0700

    heheh

--- resources/views/oa/dashboard.blade.php
+++ resources/views/oa/dashboard.blade.php
@@ -5,14 +5,19 @@
 @endsection
 @section('css')
     <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
+    <link rel="stylesheet" href="{{asset('css/oa-dashboard.css')}}">
 @endsection
 
 @section('content')
-    <div class="content">
-        <p>Tên: {{$oa_info['name']}}</p>
-        <p>Mô tả: {{$oa_info['description']}}</p>
-        <p>Avatar: <img alt="" src="{{$oa_info['avatar']}}" class="oa-avatar"></p>
-        <p>Cover: <img alt="" src="{{$oa_info['cover']}}"></p>
+    <div class="oa-dashboard">
+        <div class="oa-dashboard-header">
+        <p class="oa-dashboard-name">{{$oa_info['name']}}</p>
+        <p class="oa-dashboard-sub-name">{$oa_info['description']}}</p>
+        </div>
+        <div class="image-container">
+            <img alt="" src="{{$oa_info['cover']}}" class="background-image">
+            <img alt="" src="{{$oa_info['avatar']}}" class="oa-image">
+        </div>
 </div>
 
 @endsection
