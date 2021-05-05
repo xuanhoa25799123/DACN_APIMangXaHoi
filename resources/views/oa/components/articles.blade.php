69de25dc06a7a2e557b40cbe4d8fae3d1a83512e
super3636 <ioeioe649764321@gmail.com>
2021-05-05 08:36:58 +0700

    heheh

--- resources/views/oa/components/articles.blade.php
+++ resources/views/oa/components/articles.blade.php
@@ -9,11 +9,11 @@
 @section('content')
         @foreach($articles as $article)
             <p>{{$article['title']}}</p>
-    <img src="{{$article['thumb']}}" alt="">
-    <p>Lượt xem: {{$article['total_view']}}</p>
-    <p>Lượt chia sẻ: {{$article['total_share']}}</p>
-    <p>Trạng thái: {{$article['status']}}</p>
-    <p>Ngày đăng: {{date('d/m/Y',$article['create_date'])}}</p>
+        <img src="{{$article['thumb']}}" alt="">
+        <p>Lượt xem: {{$article['total_view']}}</p>
+        <p>Lượt chia sẻ: {{$article['total_share']}}</p>
+        <p>Trạng thái: {{$article['status']}}</p>
+        <p>Ngày đăng: {{date('d/m/Y',$article['create_date'])}}</p>
             <p>Ngày chỉnh sửa gần nhất: {{date('d/m/Y',$article['update_date'])}}</p>
         @endforeach
 @endsection
