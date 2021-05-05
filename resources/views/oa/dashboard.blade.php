bd488f85cea67ac12233f679218017d144181e29
super3636 <ioeioe649764321@gmail.com>
2021-05-05 08:30:17 +0700

    heheh

--- resources/views/oa/dashboard.blade.php
+++ resources/views/oa/dashboard.blade.php
@@ -12,12 +12,12 @@
     <div class="oa-dashboard">
         <div class="oa-dashboard-header">
         <p class="oa-dashboard-name">{{$oa_info['name']}}</p>
-        <p class="oa-dashboard-sub-name">{$oa_info['description']}}</p>
         </div>
         <div class="image-container">
             <img alt="" src="{{$oa_info['cover']}}" class="background-image">
             <img alt="" src="{{$oa_info['avatar']}}" class="oa-image">
         </div>
+        <p class="oa-dashboard-sub-name">{{$oa_info['description']}}</p>
 </div>
 
 @endsection
