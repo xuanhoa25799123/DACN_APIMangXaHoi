a09d630787f634efebf42bb4354b7584c9d486e2
super3636 <ioeioe649764321@gmail.com>
2021-05-07 13:05:40 +0700

    hehe

--- resources/views/oa/components/followers.blade.php
+++ resources/views/oa/components/followers.blade.php
@@ -37,13 +37,4 @@
         @endforeach
         </tbody>
     </table>
-    @foreach($followers as $follower)
-        <img src="{{$follower['avatar']}}" alt="">
-        <p>Giới tính:    @if($follower['user_gender']=="1")
-            <p>GT: Nam</p>
-        @else
-            <p>GT: Nữ</p>
-            @endif</p>
-        <p>Name: {{$follower['display_name']}}</p>
-    @endforeach
 @endsection
