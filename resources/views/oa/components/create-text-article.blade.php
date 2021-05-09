c280382a71f6677b36039b9a7d996e623fbdeb0a
super3636 <ioeioe649764321@gmail.com>
2021-05-09 09:16:44 +0700

    hehe

--- resources/views/oa/components/create-text-article.blade.php
+++ resources/views/oa/components/create-text-article.blade.php
@@ -1,30 +1,39 @@
 @extends('oa.layouts.admin')
 
 @section('css')
-    <link rel="stylesheet" href="{{asset('/css/article.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/create-article.css')}}">
 
 @endsection
 @section('js')
 
 @endsection
 @section('content')
+    <div class="create-container">
     <form action="" method="POST">
-        <div class="form-group form-inline">
-            <label>Tiêu đề (*)</label>
+        <div class="left-content">
+        <div class="form-group inline-form">
+            <label>Tiêu đề</label>
             <input class="form-control" name="title" placeholder="Tiêu đề bài viết" maxlength="50">
         </div>
-        <div class="form-group form-inline">
+        <div class="form-group inline-form">
             <label>Trích dẫn</label>
             <textarea class="form-control" rows="5" maxlength="300"></textarea>
         </div>
-        <div class="form-group form-inline">
+        <div class="form-group inline-form">
             <label>Tác giả</label>
             <input class="form-control" name="author">
         </div>
-        <div class="form-group form-inline">
+        <div class="form-group inline-form">
             <label>Nội dung</label>
             <textarea class="form-control" name="content" rows="10"></textarea>
         </div>
+        </div>
+        <div class="right-content">
+            <div class="form-group">
+                <label>Chọn ảnh đại diện</label>
+            </div>
+        </div>
     </form>
+    </div>
 
 @endsection
