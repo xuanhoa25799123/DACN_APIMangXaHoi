1062f3ce701996db11bbde9003b6f6241046db32
super3636 <ioeioe649764321@gmail.com>
2021-05-11 15:36:22 +0700

    hehe

--- public/js/create-article-2.js
+++ public/js/create-article-2.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
     $('.image-input').on('change',function(e) {
-        console.log(asdsad);
         let src = $(this).val();
+        if(src=="")
+        {
+              $('.image-info').css('display','flex');
+
+        }
+        else{
         $('.image-info').css('display','none');
         $('.preview-image').attr('src',src);
+        }
     });   
     $('.video-button').on('click',function(){
         $(this).addClass('active-button');
         $('.image-button').removeClass('active-button');
         $('.video-content').css('display','flex');
-        $('.image-content').css('display','initial');
+        $('.image-content').css('display','none');
     })
      $('.image-button').on('click',function(){
         $(this).addClass('active-button');
