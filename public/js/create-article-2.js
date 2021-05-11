5d9ea4c95f4d3a12f1b7f7de1e124da5646ee346
super3636 <ioeioe649764321@gmail.com>
2021-05-11 17:16:53 +0700

    hehe

--- public/js/create-article-2.js
+++ public/js/create-article-2.js
@@ -26,6 +26,38 @@ $(document).ready(function() {
     })
     $('.video-info').on('click',function(){
         $('.video-popup').css('display','flex');
+        // $.ajax({
+        //     url: "https://openapi.zalo.me/v2.0/article/getslice",
+        //     type: 'POST',
+        //     data:{
+        //         link,message
+        //     },
+        //     headers:{
+        //         'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+        //     },
+        //     dataType: 'json',
+        //     success: function(response) {
+        //         Swal.fire({
+        //             title: 'Thành công',
+        //             text: "Đã đăng bài viết "+response.message + " "+response.link,
+        //             icon: 'success',
+        //             showCancelButton: true,
+        //             confirmButtonColor: '#3085d6',
+        //             cancelButtonColor: '#6c800b',
+        //             confirmButtonText:"Trở về trang chủ",
+        //             cancelButtonText:"Ở lại",
+        //         }).then(result=>{
+        //             if(result.isConfirmed)
+        //             {
+        //                 window.location.href="/dashboard";
+        //             }
+        //             else
+        //             {
+        //                 $(".preview-container").css('display','none');
+        //             }
+        //         })
+        //     }
+        // });
         
     })
     $('.close-video-popup').on('click',function(){
