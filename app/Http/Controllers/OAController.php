cbc66c325aaeb2a4c4ebaf7672dc57da9f2f823d
super3636 <ioeioe649764321@gmail.com>
2021-05-17 17:51:59 +0700

    hehe

--- app/Http/Controllers/OAController.php
+++ app/Http/Controllers/OAController.php
@@ -351,7 +351,7 @@ class OAController extends Controller
          {
              $videos = session('videos');
              array_filter($videos,function($video){
-                 return $video->id!=$id
+                 return $video->id!=$id;
              });
              session(['videos'=>$videos]);
              return response()->json(['success'=>true]);
