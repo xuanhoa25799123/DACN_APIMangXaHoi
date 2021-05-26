7499f2d4fd72b78a91e2153e7b5f26021f09f50a
super3636 <ioeioe649764321@gmail.com>
2021-05-26 15:15:56 +0700

    hhe

--- public/js/follower-chat.js
+++ public/js/follower-chat.js
@@ -1,39 +1,41 @@
 $(document).ready(function () {
     $(".message-enter").keypress(function (e) {
-        e.preventDefault();
-        let message = $(this).val();
-        let user_id = $(this).data("id");
-        let url = $(this).data("href");
-        console.log(message, user_id, url);
-        if ($.trim(message) == "") {
-            return;
-        } else {
-            $.ajax({
-                url: url,
-                type: "POST",
-                data: {
-                    message,
-                    user_id,
-                },
-                headers: {
-                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
-                        "content"
-                    ),
-                },
-                dataType: "json",
-                success: function (response) {
-                    if (response.message == "Success") {
-                    } else {
-                        Swal.fire({
-                            title: "Tin nhắn chưa được gửi",
-                            text: response.message,
-                            icon: "warning",
-                            showCancelButton: true,
-                            confirmButtonColor: "#3085d6",
-                        });
-                    }
-                },
-            });
+        if (e.which == 13) {
+            e.preventDefault();
+            let message = $(this).val();
+            let user_id = $(this).data("id");
+            let url = $(this).data("href");
+            console.log(message, user_id, url);
+            if ($.trim(message) == "") {
+                return;
+            } else {
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    data: {
+                        message,
+                        user_id,
+                    },
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                            "content"
+                        ),
+                    },
+                    dataType: "json",
+                    success: function (response) {
+                        if (response.message == "Success") {
+                        } else {
+                            Swal.fire({
+                                title: "Tin nhắn chưa được gửi",
+                                text: response.message,
+                                icon: "warning",
+                                showCancelButton: true,
+                                confirmButtonColor: "#3085d6",
+                            });
+                        }
+                    },
+                });
+            }
         }
     });
 });
