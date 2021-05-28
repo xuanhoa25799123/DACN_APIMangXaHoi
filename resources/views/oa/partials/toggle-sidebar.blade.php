b44164b22efd517c6352bd1eb2505bd45cabf951
super3636 <ioeioe649764321@gmail.com>
2021-05-28 13:56:28 +0700

    hehe

--- resources/views/oa/partials/toggle-sidebar.blade.php
+++ resources/views/oa/partials/toggle-sidebar.blade.php
@@ -1,51 +1,51 @@
-<button class="toggle-sidebar-button"><i class="fas fa-bars fa-1x"></i></button>
 
-<div class="toggle-toggle-sidebar">
-    <div class="toggle-toggle-sidebar-logo-container">
-        <a class="toggle-toggle-sidebar-logo" href="/admin">
-            <p class="toggle-toggle-sidebar-logo-text">Zalo API</p>
+<button class="toggle-sidebar-button"><i class="fas fa-bars fa-1x"></i></button>
+<div class="toggle-sidebar">
+    <div class="toggle-sidebar-logo-container">
+        <a class="toggle-sidebar-logo" href="/admin">
+            <p class="toggle-sidebar-logo-text">Zalo API</p>
         </a>
     </div>
 
-    <div class="toggle-toggle-sidebar-content">
-        <p class="toggle-toggle-sidebar-content-header">Zalo doanh nghiệp</p>
-        <a class="toggle-toggle-sidebar-item" href="{{route('oa-home')}}">
-            <div class="toggle-toggle-sidebar-item-icon-container">
-                <i class="fa fa-home toggle-toggle-sidebar-item-icon"></i>
+    <div class="toggle-sidebar-content">
+        <p class="toggle-sidebar-content-header">Zalo doanh nghiệp</p>
+        <a class="toggle-sidebar-item" href="{{route('oa-home')}}">
+            <div class="toggle-sidebar-item-icon-container">
+                <i class="fa fa-home toggle-sidebar-item-icon"></i>
             </div>
-            <p class='toggle-toggle-sidebar-item-text'>Trang chủ</p>
+            <p class='toggle-sidebar-item-text'>Trang chủ</p>
         </a>
-        <a class="toggle-toggle-sidebar-item" href="{{route('oa-broadcast')}}">
-            <div class="toggle-toggle-sidebar-item-icon-container">
-                <i class="fa fa-rss toggle-toggle-sidebar-item-icon"></i>
+        <a class="toggle-sidebar-item" href="{{route('oa-broadcast')}}">
+            <div class="toggle-sidebar-item-icon-container">
+                <i class="fa fa-rss toggle-sidebar-item-icon"></i>
             </div>
-            <p class='toggle-toggle-sidebar-item-text'>Gửi broadcast</p>
+            <p class='toggle-sidebar-item-text'>Gửi broadcast</p>
         </a>
-        <a class="toggle-toggle-sidebar-item" href="{{route('oa-list')}}">
-            <div class="toggle-toggle-sidebar-item-icon-container">
-                <i class="fa fa-users toggle-toggle-sidebar-item-icon"></i>
+        <a class="toggle-sidebar-item" href="{{route('oa-list')}}">
+            <div class="toggle-sidebar-item-icon-container">
+                <i class="fa fa-users toggle-sidebar-item-icon"></i>
             </div>
-            <p class='toggle-toggle-sidebar-item-text'>Danh sách người quan tâm</p>
+            <p class='toggle-sidebar-item-text'>Danh sách người quan tâm</p>
         </a>
-        <a class="toggle-toggle-sidebar-item" href="{{route('oa-article')}}">
-            <div class="toggle-toggle-sidebar-item-icon-container">
-                <i class="fa fa-pencil toggle-toggle-sidebar-item-icon"></i>
+        <a class="toggle-sidebar-item" href="{{route('oa-article')}}">
+            <div class="toggle-sidebar-item-icon-container">
+                <i class="fa fa-pencil toggle-sidebar-item-icon"></i>
             </div>
-            <p class='toggle-toggle-sidebar-item-text'>Danh sách bài viết</p>
+            <p class='toggle-sidebar-item-text'>Danh sách bài viết</p>
         </a>
-        <a class="toggle-toggle-sidebar-item" href="{{route('oa-video')}}">
-            <div class="toggle-toggle-sidebar-item-icon-container">
-                <i class="fa fa-video toggle-toggle-sidebar-item-icon"></i>
+        <a class="toggle-sidebar-item" href="{{route('oa-video')}}">
+            <div class="toggle-sidebar-item-icon-container">
+                <i class="fa fa-video toggle-sidebar-item-icon"></i>
             </div>
-            <p class='toggle-toggle-sidebar-item-text'>Danh sách video</p>
+            <p class='toggle-sidebar-item-text'>Danh sách video</p>
         </a>
 
-        <p class="toggle-toggle-sidebar-content-header">Zalo xã hội</p>
-        <a class="toggle-toggle-sidebar-item" href="/dashboard">
-            <div class="toggle-toggle-sidebar-item-icon-container">
-                <i class="fa fa-arrow-left toggle-toggle-sidebar-item-icon"></i>
+        <p class="toggle-sidebar-content-header">Zalo xã hội</p>
+        <a class="toggle-sidebar-item" href="/dashboard">
+            <div class="toggle-sidebar-item-icon-container">
+                <i class="fa fa-arrow-left toggle-sidebar-item-icon"></i>
             </div>
-            <p class='toggle-toggle-sidebar-item-text'>Quay lại</p>
+            <p class='toggle-sidebar-item-text'>Quay lại</p>
         </a>
     </div>
 
