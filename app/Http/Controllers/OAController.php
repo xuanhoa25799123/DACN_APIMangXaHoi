0a2bb9792d3df44df3d9e9735a6f14aab1c38a23
super3636 <ioeioe649764321@gmail.com>
2021-05-30 10:47:17 +0700

    hehe

--- app/Http/Controllers/OAController.php
+++ app/Http/Controllers/OAController.php
@@ -986,8 +986,8 @@ class OAController extends Controller
                     'attachment_id'=>$id,
                 ]);
             }
-
-          $accessToken = 'AEma5D8bHXGgwoWNrY1F4o7cAnVSDpHNO_in2UbkAajamqyYtn5NIoMsDHYd85LdCP4p3yygK4nifKCmaor2DJocH4x3ArOuUwvCRTmxJMf2laewk0LEEmoELKAM0KmTDgj9Gi4CB0TNlZLXsWjK3o63HL2rEKOLA95qRwajPWWWucLXZLvc91l5Nrc53L4vBR0oLx5w4J0KpGTgcm0qFnkCEagp1mmTAwmLVxyv7Iy6XovF-40T2mNC3a_uL3iGHjm6MEv0CX5XyL1kspOg5tomB59rOix_Lp7LFK4S';
+              $accessToken = session('oa_token');
+        
           $data=json_encode([
               'recipient'=>[
                   'target'=>[
@@ -1019,8 +1019,8 @@ class OAController extends Controller
 }
     public function uploadingVideo(Request $request)
     {
-            $video = $request->file;
-         $accessToken = session('oa-token');
+         $accessToken = session('oa_token');
+            return response()->json(['success'=>true,'result'=>$request->all()]);
          $client = new \GuzzleHttp\Client();
                  $response = $client->request('POST','https://openapi.zalo.me/v2.0/article/upload_video/preparevideo',['query'=>[
                'access_token'=>$accessToken
