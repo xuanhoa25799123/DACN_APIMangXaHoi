e0905e11bb401df4101bae1e5a34901c4e2ccde1
super3636 <ioeioe649764321@gmail.com>
2021-05-31 08:49:38 +0700

    hehe

--- public/js/test/upload-video.js
+++ public/js/test/upload-video.js
@@ -29,24 +29,24 @@ $(document).ready(function () {
             formData.append("name", "file");
             formData.append("filename", file.name);
             console.log(file);
-            let access_token =
+            let accessToken =
                 "AEma5D8bHXGgwoWNrY1F4o7cAnVSDpHNO_in2UbkAajamqyYtn5NIoMsDHYd85LdCP4p3yygK4nifKCmaor2DJocH4x3ArOuUwvCRTmxJMf2laewk0LEEmoELKAM0KmTDgj9Gi4CB0TNlZLXsWjK3o63HL2rEKOLA95qRwajPWWWucLXZLvc91l5Nrc53L4vBR0oLx5w4J0KpGTgcm0qFnkCEagp1mmTAwmLVxyv7Iy6XovF-40T2mNC3a_uL3iGHjm6MEv0CX5XyL1kspOg5tomB59rOix_Lp7LFK4S";
             $.ajax({
                 url:
-                    "https://openapi.zalo.me/v2.0/article/upload_video/preparevideo?access_token=" +
-                    access_token,
-                type: "POST",
-                contentType: false,
-                enctype: "multipart/form-data",
-                processData: false,
-                data: formData,
-                crossDomain: true,
-
+                    "https://openapi.zalo.me/v2.0/article/getslice?offset=0&limit=5&type=normal&access_token=" +
+                    accessToken,
+                type: "GET",
+                // contentType: false,
+                // enctype: "multipart/form-data",
+                // processData: false,
+                // data: formData,
+                dataType: "jsonp",
                 headers: {
                     "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
                         "content"
                     ),
-                    "Access-Control-Allow-Origin": "http://localhost:8000",
+                    "Access-Control-Allow-Headers":
+                        "X-CSRF-Token, Content-Type",
                 },
                 success: function (response) {
                     console.log(response);
