e2d6de592bd0213627c22c869d92bdba251b036a
super3636 <ioeioe649764321@gmail.com>
2021-06-09 11:22:48 +0700

    hehe

--- app/Http/Controllers/ZaloController.php
+++ app/Http/Controllers/ZaloController.php
@@ -250,13 +250,13 @@ class ZaloController extends Controller
         $params = ['message' => $request->message, 'link' => $request->link];
         $response = $this->zalo->post(ZaloEndpoint::API_GRAPH_POST_FEED, $accessToken, $params);
         $result = $response->getDecodedBody();
-        // if(!empty($result['id']))
-        // {
+        if(!empty($result['id']))
+        {
         return response()->json(['message'=>'Success']);
-        // }
-        // else{
-        //       return response()->json(['message'=>'Xảy ra lỗi thử lại sau','result'=>$result]);
-        // }
+        }
+        else{
+              return response()->json(['message'=>$response['message']]);
+        }
     }
     public function extractProcess(Request $request)
     {
